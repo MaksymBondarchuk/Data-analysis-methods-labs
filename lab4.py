4cbc9c44dd10ce5de0c6de74a2c18db8d47cd75d
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2019-11-24 13:01:41 +0200

    Lab 4. Part 1

--- lab4.py
+++ lab4.py
@@ -9,13 +9,30 @@ print(business.head())
 print(business.info())
 
 drop_cols = ["address", "attributes", "hours", "is_open", "latitude", "longitude", "name", "postal_code"]
-# # del business[drop_cols]
-# business = business.drop(drop_cols, axis=1)
-# # business = business.drop(["address", "attributes"], axis=1)
-# print(business.head())
-# print(business.info())
-# print(business['categories'])
-# category_counts = business['categories'].apply(pd.Series).stack().reset_index(drop=True).value_counts().nlargest(20)
-# print(category_counts)
-# print(type(category_counts))
-# category_counts.plot(kind='bar')
+
+business = business.drop(drop_cols, axis=1)
+
+print(business.head())
+print(business.categories[0])
+print(type(business.categories[0]))
+print(business.categories[0][0])
+business['categories'] = business['categories'].str.split(', ')
+print(business.categories[0])
+print(business['categories'].head())
+
+print(business.shape)
+print(pd.isna(business['categories']).sum())
+business = business[~pd.isna(business['categories'])]
+print(business.head())
+print(business.shape)
+print(pd.isna(business['categories']).sum())
+
+business = business.loc[business['categories'].apply(lambda x: 'Restaurants' in x), :]
+
+# Збережіть потрібний business ID у файл
+italian_pizza = business.loc[business['categories'].apply(lambda x: 'Pizza' in x or 'Italian' in x), :]
+print(italian_pizza.head())
+print(italian_pizza.shape)
+
+# print(italian_pizza.info())
+italian_pizza.to_csv(path_or_buf='italian_pizza.csv', index=False, columns=('business_id', 'review_count', 'stars', 'state'))
